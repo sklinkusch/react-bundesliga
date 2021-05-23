7858d9c164e8a7d5d538f81c4d49383af7bdad8a
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-05-23 16:44:01 +0200

    don't import context

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import AppProvider from "./context/AppProvider"
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppProvider>
       <App />
-    </AppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
