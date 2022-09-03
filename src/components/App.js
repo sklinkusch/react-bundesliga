d7bf0eaa354c1d8534dfb73f63594ba9008bc2e9
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-09-03 13:12:28 +0200

    change App.js to previous changes accordingly

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
 /** @jsxImportSource theme-ui */
 import React from "react"
-import Matches from "./Matches"
-import Table from "./Table"
+import Liga12223 from "../pages/Liga1-22-23";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Liga22223 from "../pages/Liga2-22-23";
 
 function App() {
   return (
     <div className="App" sx={{ textAlign: "center" }}>
-      <header className="App-header" sx={{ display: "grid", gridTemplateColumns: "250px 1fr", columnGap: "20px" }}>
-        <Matches />
-        <Table />
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/liga1/22-23" element={<Liga12223 />} />
+          <Route path="/liga1/*" element={<Liga12223 />} />
+          <Route path="/liga2/22-23" element={<Liga22223 />} />
+          <Route path="/liga2/*" element={<Liga22223 />} />
+          <Route path="" element={<Liga12223 />} />
+          <Route path="*" element={<Liga12223 />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
