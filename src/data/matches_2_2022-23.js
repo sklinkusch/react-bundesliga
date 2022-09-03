200ca163f925682830099acbf32136e7967d09ce
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-09-03 13:12:11 +0200

    matches (day 1-14) for 2nd league (season 2022/23)

---
+++ src/data/matches_2_2022-23.js
@@ -0,0 +1,556 @@
+const matches = {
+  1: [
+    {
+      teams: ["FCK", "H96"],
+      goals: [2, 1]
+    },
+    {
+      teams: ["SGF", "KIE"],
+      goals: [2, 2]
+    },
+    {
+      teams: ["STP", "FCN"],
+      goals: [3, 2]
+    },
+    {
+      teams: ["SVS", "BIE"],
+      goals: [2, 1]
+    },
+    {
+      teams: ["JRE", "D98"],
+      goals: [2, 0]
+    },
+    {
+      teams: ["FCM", "DÜS"],
+      goals: [1, 2]
+    },
+    {
+      teams: ["P07", "KSC"],
+      goals: [5, 0]
+    },
+    {
+      teams: ["ROS", "FCH"],
+      goals: [0, 1]
+    },
+    {
+      teams: ["EBS", "HSV"],
+      goals: [0, 2]
+    }
+  ],
+  2: [
+    {
+      teams: ["D98", "SVS"],
+      goals: [2, 1]
+    },
+    {
+      teams: ["DÜS", "P07"],
+      goals: [2, 1]
+    },
+    {
+      teams: ["FCH", "EBS"],
+      goals: [3, 0]
+    },
+    {
+      teams: ["FCN", "SGF"],
+      goals: [2, 0]
+    },
+    {
+      teams: ["KIE", "FCK"],
+      goals: [2, 2]
+    },
+    {
+      teams: ["H96", "STP"],
+      goals: [2, 2]
+    },
+    {
+      teams: ["BIE", "JRE"],
+      goals: [0, 3]
+    },
+    {
+      teams: ["HSV", "ROS"],
+      goals: [0, 1]
+    },
+    {
+      teams: ["KSC", "FCM"],
+      goals: [2, 3]
+    }
+  ],
+  3: [
+    {
+      teams: ["SGF", "KSC"],
+      goals: [1, 1]
+    },
+    {
+      teams: ["SVS", "DÜS"],
+      goals: [1, 0]
+    },
+    {
+      teams: ["HSV", "FCH"],
+      goals: [1, 0]
+    },
+    {
+      teams: ["P07", "H96"],
+      goals: [4, 2]
+    },
+    {
+      teams: ["JRE", "FCN"],
+      goals: [0, 0]
+    },
+    {
+      teams: ["ROS", "BIE"],
+      goals: [2, 1]
+    },
+    {
+      teams: ["FCM", "KIE"],
+      goals: [1, 2]
+    },
+    {
+      teams: ["EBS", "D98"],
+      goals: [0, 1]
+    },
+    {
+      teams: ["FCK", "STP"],
+      goals: [2, 1]
+    }
+  ],
+  4: [
+    {
+      teams: ["FCN", "FCH"],
+      goals: [0, 3]
+    },
+    {
+      teams: ["FCK", "P07"],
+      goals: [0, 1],
+    },
+    {
+      teams: ["BIE", "HSV"],
+      goals: [0, 2]
+    },
+    {
+      teams: ["KIE", "EBS"],
+      goals: [3, 0]
+    },
+    {
+      teams: ["KSC", "SVS"],
+      goals: [3, 2]
+    },
+    {
+      teams: ["D98", "ROS"],
+      goals: [4, 0]
+    },
+    {
+      teams: ["STP", "FCM"],
+      goals: [3, 0]
+    },
+    {
+      teams: ["DÜS", "SGF"],
+      goals: [2, 2]
+    },
+    {
+      teams: ["H96", "JRE"],
+      goals: [1, 0]
+    }
+  ],
+  5: [
+    {
+      teams: ["HSV", "D98"],
+      goals: [1, 2]
+    },
+    {
+      teams: ["FCM", "H96"],
+      goals: [0, 4]
+    },
+    {
+      teams: ["P07", "KIE"],
+      goals: [7, 2]
+    },
+    {
+      teams: ["SVS", "FCN"],
+      goals: [1, 2]
+    },
+    {
+      teams: ["JRE", "KSC"],
+      goals: [0, 6]
+    },
+    {
+      teams: ["EBS", "DÜS"],
+      goals: [2, 2]
+    },
+    {
+      teams: ["SGF", "FCK"],
+      goals: [1, 3]
+    },
+    {
+      teams: ["FCH", "BIE"],
+      goals: [1, 1]
+    },
+    {
+      teams: ["ROS", "STP"],
+      goals: [2, 0]
+    }
+  ],
+  6: [
+    {
+      teams: ["BIE", "EBS"],
+      goals: [4, 1]
+    },
+    {
+      teams: ["DÜS", "JRE"],
+      goals: [4, 0]
+    },
+    {
+      teams: ["D98", "FCH"],
+      goals: [2, 2]
+    },
+    {
+      teams: ["STP", "P07"],
+      goals: [2, 2]
+    },
+    {
+      teams: ["KSC", "ROS"],
+      goals: [2, 0]
+    },
+    {
+      teams: ["FCN", "HSV"],
+      goals: [0, 2]
+    },
+    {
+      teams: ["KIE", "SVS"],
+      goals: [1, 0]
+    },
+    {
+      teams: ["H96", "SGF"],
+      goals: [2, 1]
+    },
+    {
+      teams: ["FCK", "FCM"],
+      goals: [4, 4]
+    }
+  ],
+  7: [
+    {
+      teams: ["FCH", "DÜS"],
+      goals: [2, 1]
+    },
+    {
+      teams: ["EBS", "FCN"],
+      goals: [4, 2]
+    },
+    {
+      teams: ["SGF", "STP"],
+      goals: [null, null]
+    },
+    {
+      teams: ["P07", "FCM"],
+      goals: [null, null]
+    },
+    {
+      teams: ["JRE", "KIE"],
+      goals: [null, null]
+    },
+    {
+      teams: ["HSV", "KSC"],
+      goals: [null, null]
+    },
+    {
+      teams: ["D98", "BIE"],
+      goals: [null, null]
+    },
+    {
+      teams: ["ROS", "H96"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SVS", "FCK"],
+      goals: [null, null]
+    }
+  ],
+  8: [
+    {
+      teams: ["FCN", "BIE"],
+      goals: [null, null]
+    },
+    {
+      teams: ["KIE", "HSV"],
+      goals: [null, null]
+    },
+    {
+      teams: ["P07", "JRE"],
+      goals: [null, null]
+    },
+    {
+      teams: ["H96", "EBS"],
+      goals: [null, null]
+    },
+    {
+      teams: ["KSC", "FCH"],
+      goals: [null, null]
+    },
+    {
+      teams: ["DÜS", "ROS"],
+      goals: [null, null]
+    },
+    {
+      teams: ["STP", "SVS"],
+      goals: [null, null]
+    },
+    {
+      teams: ["FCM", "SGF"],
+      goals: [null, null]
+    },
+    {
+      teams: ["FCK", "D98"],
+      goals: [null, null]
+    }
+  ],
+  9: [
+    {
+      teams: ["SVS", "H96"],
+      goals: [null, null]
+    },
+    {
+      teams: ["EBS", "KSC"],
+      goals: [null, null]
+    },
+    {
+      teams: ["BIE", "KIE"],
+      goals: [null, null]
+    },
+    {
+      teams: ["D98", "FCN"],
+      goals: [null, null]
+    },
+    {
+      teams: ["ROS", "FCM"],
+      goals: [null, null]
+    },
+    {
+      teams: ["HSV", "DÜS"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SGF", "P07"],
+      goals: [null, null]
+    },
+    {
+      teams: ["FCH", "FCK"],
+      goals: [null, null]
+    },
+    {
+      teams: ["JRE", "STP"],
+      goals: [null, null]
+    }
+  ],
+  10: [
+    {
+      teams: ["P07", "D98"],
+      goals: [null, null]
+    },
+    {
+      teams: ["H96", "HSV"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SGF", "SVS"],
+      goals: [null, null]
+    },
+    {
+      teams: ["KIE", "ROS"],
+      goals: [null, null]
+    },
+    {
+      teams: ["DÜS", "BIE"],
+      goals: [null, null]
+    },
+    {
+      teams: ["STP", "FCH"],
+      goals: [null, null]
+    },
+    {
+      teams: ["KSC", "FCN"],
+      goals: [null, null]
+    },
+    {
+      teams: ["FCM", "JRE"],
+      goals: [null, null]
+    },
+    {
+      teams: ["FCK", "EBS"],
+      goals: [null, null]
+    }
+  ],
+  11: [
+    {
+      teams: ["BIE", "KSC"],
+      goals: [null, null]
+    },
+    {
+      teams: ["JRE", "SGF"],
+      goals: [null, null]
+    },
+    {
+      teams: ["D98", "DÜS"],
+      goals: [null, null]
+    },
+    {
+      teams: ["ROS", "P07"],
+      goals: [null, null]
+    },
+    {
+      teams: ["EBS", "STP"],
+      goals: [null, null]
+    },
+    {
+      teams: ["HSV", "FCK"],
+      goals: [null, null]
+    },
+    {
+      teams: ["FCH", "H96"],
+      goals: [null, null]
+    },
+    {
+      teams: ["FCN", "KIE"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SVS", "FCM"],
+      goals: [null, null]
+    }
+  ],
+  12: [
+    {
+      teams: ["SGF", "ROS"],
+      goals: [null, null]
+    },
+    {
+      teams: ["STP", "HSV"],
+      goals: [null, null]
+    },
+    {
+      teams: ["P07", "SVS"],
+      goals: [null, null]
+    },
+    {
+      teams: ["KIE", "FCH"],
+      goals: [null, null]
+    },
+    {
+      teams: ["DÜS", "FCN"],
+      goals: [null, null]
+    },
+    {
+      teams: ["H96", "BIE"],
+      goals: [null, null]
+    },
+    {
+      teams: ["KSC", "D98"],
+      goals: [null, null]
+    },
+    {
+      teams: ["FCM", "EBS"],
+      goals: [null, null]
+    },
+    {
+      teams: ["FCK", "JRE"],
+      goals: [null, null]
+    }
+  ],
+  13: [
+    {
+      teams: ["BIE", "STP"],
+      goals: [null, null]
+    },
+    {
+      teams: ["HSV", "FCM"],
+      goals: [null, null]
+    },
+    {
+      teams: ["D98", "KIE"],
+      goals: [null, null]
+    },
+    {
+      teams: ["FCH", "SGF"],
+      goals: [null, null]
+    },
+    {
+      teams: ["FCN", "H96"],
+      goals: [null, null]
+    },
+    {
+      teams: ["KSC", "DÜS"],
+      goals: [null, null]
+    },
+    {
+      teams: ["ROS", "FCK"],
+      goals: [null, null]
+    },
+    {
+      teams: ["JRE", "SVS"],
+      goals: [null, null]
+    },
+    {
+      teams: ["EBS", "P07"],
+      goals: [null, null]
+    }
+  ],
+  14: [
+    {
+      teams: ["SGF", "BIE"],
+      goals: [null, null]
+    },
+    {
+      teams: ["STP", "D98"],
+      goals: [null, null]
+    },
+    {
+      teams: ["P07", "HSV"],
+      goals: [null, null]
+    },
+    {
+      teams: ["KIE", "DÜS"],
+      goals: [null, null]
+    },
+    {
+      teams: ["H96", "KSC"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SVS", "EBS"],
+      goals: [null, null]
+    },
+    {
+      teams: ["JRE", "ROS"],
+      goals: [null, null]
+    },
+    {
+      teams: ["FCM", "FCH"],
+      goals: [null, null]
+    },
+    {
+      teams: ["FCK", "FCN"],
+      goals: [null, null]
+    }
+  ],
+  15: [],
+  16: [],
+  17: [],
+  18: [],
+  19: [],
+  20: [],
+  21: [],
+  22: [],
+  23: [],
+  24: [],
+  25: [],
+  26: [],
+  27: [],
+  28: [],
+  29: [],
+  30: [],
+  31: [],
+  32: [],
+  33: [],
+  34: []
+}
+
+export default matches
