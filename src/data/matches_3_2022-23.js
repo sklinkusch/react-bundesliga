a5ab852261fe5465485eba6f9b9901efc2f4b55a
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-09-04 11:28:26 +0200

    add matches of first 7 days of Liga3

---
+++ src/data/matches_3_2022-23.js
@@ -0,0 +1,329 @@
+const matches = {
+  1: [
+    {
+      teams: ["OSN", "DUI"],
+      goals: [1, 0]
+    },
+    {
+      teams: ["OLD", "SVM"],
+      goals: [1, 1]
+    },
+    {
+      teams: ["FCI", "BAY"],
+      goals: [1, 0]
+    },
+    {
+      teams: ["WMA", "VKÖ"],
+      goals: [3, 1]
+    },
+    {
+      teams: ["RWE", "SVE"],
+      goals: [1, 5]
+    },
+    {
+      teams: ["DRE", "MÜN"],
+      goals: [3, 4]
+    },
+    {
+      teams: ["FCS", "SCV"],
+      goals: [1, 0]
+    },
+    {
+      teams: ["SCF2", "AUE"],
+      goals: [1, 1]
+    },
+    {
+      teams: ["ZWI", "HFC"],
+      goals: [3, 2]
+    },
+    {
+      teams: ["WIE", "BVB2"],
+      goals: [1, 1]
+    }
+  ],
+  2: [
+    {
+      teams: ["DUI", "RWE"],
+      goals: [2, 2]
+    },
+    {
+      teams: ["MÜN", "OLD"],
+      goals: [1, 0]
+    },
+    {
+      teams: ["SVE", "FCS"],
+      goals: [0, 2]
+    },
+    {
+      teams: ["VKÖ", "WIE"],
+      goals: [1, 0]
+    },
+    {
+      teams: ["SVM", "ZWI"],
+      goals: [3, 0]
+    },
+    {
+      teams: ["HFC", "DRE"],
+      goals: [0, 2]
+    },
+    {
+      teams: ["BAY", "SCF2"],
+      goals: [0, 1]
+    },
+    {
+      teams: ["BVB2", "FCI"],
+      goals: [0, 4]
+    },
+    {
+      teams: ["AUE", "OSN"],
+      goals: [1, 1]
+    },
+    {
+      teams: ["SCV", "WMA"],
+      goals: [2, 2]
+    }
+  ],
+  3: [
+    {
+      teams: ["OLD", "SVE"],
+      goals: [2, 3]
+    },
+    {
+      teams: ["MÜN", "SVM"],
+      goals: [4, 0]
+    },
+    {
+      teams: ["RWE", "VKÖ"],
+      goals: [1, 4]
+    },
+    {
+      teams: ["ZWI", "DUI"],
+      goals: [0, 1]
+    },
+    {
+      teams: ["SCF2", "HFC"],
+      goals: [2, 0]
+    },
+    {
+      teams: ["WIE", "BAY"],
+      goals: [4, 1]
+    },
+    {
+      teams: ["OSN", "FCI"],
+      goals: [0, 1]
+    },
+    {
+      teams: ["WMA", "AUE"],
+      goals: [1, 0]
+    },
+    {
+      teams: ["DRE", "SCV"],
+      goals: [2, 0]
+    },
+    {
+      teams: ["FCS", "BVB2"],
+      goals: [1, 0]
+    }
+  ],
+  4: [
+    {
+      teams: ["HFC", "OLD"],
+      goals: [2, 0]
+    },
+    {
+      teams: ["FCI", "FCS"],
+      goals: [0, 0]
+    },
+    {
+      teams: ["SVE", "ZWI"],
+      goals: [5, 0]
+    },
+    {
+      teams: ["BVB2", "RWE"],
+      goals: [1, 0]
+    },
+    {
+      teams: ["VKÖ", "DRE"],
+      goals: [2, 1]
+    },
+    {
+      teams: ["SCV", "MÜN"],
+      goals: [0, 1]
+    },
+    {
+      teams: ["BAY", "OSN"],
+      goals: [1, 0]
+    },
+    {
+      teams: ["SVM", "WMA"],
+      goals: [6, 2]
+    },
+    {
+      teams: ["AUE", "WIE"],
+      goals: [1, 5]
+    },
+    {
+      teams: ["DUI", "SCF2"],
+      goals: [3, 1]
+    }
+  ],
+  5: [
+    {
+      teams: ["MÜN", "HFC"],
+      goals: [3, 1]
+    },
+    {
+      teams: ["WIE", "OSN"],
+      goals: [1, 1]
+    },
+    {
+      teams: ["WMA", "BVB2"],
+      goals: [2, 1]
+    },
+    {
+      teams: ["RWE", "FCI"],
+      goals: [2, 2]
+    },
+    {
+      teams: ["DRE", "SVE"],
+      goals: [2, 3]
+    },
+    {
+      teams: ["FCS", "AUE"],
+      goals: [0, 0]
+    },
+    {
+      teams: ["SCF2", "VKÖ"],
+      goals: [1, 0]
+    },
+    {
+      teams: ["OLD", "SCV"],
+      goals: [1, 0]
+    },
+    {
+      teams: ["SVM", "DUI"],
+      goals: [0, 3]
+    },
+    {
+      teams: ["ZWI", "BAY"],
+      goals: [2, 0]
+    }
+  ],
+  6: [
+    {
+      teams: ["HFC", "SVM"],
+      goals: [1, 1]
+    },
+    {
+      teams: ["OSN", "FCS"],
+      goals: [2, 2]
+    },
+    {
+      teams: ["FCI", "WIE"],
+      goals: [2, 3]
+    },
+    {
+      teams: ["SVE", "WMA"],
+      goals: [1, 0]
+    },
+    {
+      teams: ["VKÖ", "MÜN"],
+      goals: [1, 1]
+    },
+    {
+      teams: ["SCV", "ZWI"],
+      goals: [3, 0]
+    },
+    {
+      teams: ["BAY", "RWE"],
+      goals: [1, 1]
+    },
+    {
+      teams: ["BVB2", "SCF2"],
+      goals: [0, 2]
+    },
+    {
+      teams: ["AUE", "DRE"],
+      goals: [0, 1]
+    },
+    {
+      teams: ["DUI", "OLD"],
+      goals: [1, 1]
+    }
+  ],
+  7: [
+    {
+      teams: ["RWE", "AUE"],
+      goals: [2, 1]
+    },
+    {
+      teams: ["OLD", "OSN"],
+      goals: [4, 3]
+    },
+    {
+      teams: ["MÜN", "DUI"],
+      goals: [4, 1]
+    },
+    {
+      teams: ["WMA", "BAY"],
+      goals: [2, 1]
+    },
+    {
+      teams: ["HFC", "SCV"],
+      goals: [5, 1]
+    },
+    {
+      teams: ["DRE", "BVB2"],
+      goals: [3, 0]
+    },
+    {
+      teams: ["SCF2", "FCI"],
+      goals: [1, 0]
+    },
+    {
+      teams: ["SVM", "SVE"],
+      goals: [null, null]
+    },
+    {
+      teams: ["ZWI", "VKÖ"],
+      goals: [null, null]
+    },
+    {
+      teams: ["FCS", "WIE"],
+      goals: [null, null]
+    }
+  ],
+  8: [],
+  9: [],
+  10: [],
+  11: [],
+  12: [],
+  13: [],
+  14: [],
+  15: [],
+  16: [],
+  17: [],
+  18: [],
+  19: [],
+  20: [],
+  21: [],
+  22: [],
+  23: [],
+  24: [],
+  25: [],
+  26: [],
+  27: [],
+  28: [],
+  29: [],
+  30: [],
+  31: [],
+  32: [],
+  33: [],
+  34: [],
+  35: [],
+  36: [],
+  37: [],
+  38: []
+}
+
+export default matches
