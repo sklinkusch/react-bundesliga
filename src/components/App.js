fd145ebc59428f6be6b8165d01debffd22ab2bb6
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-09-04 11:29:08 +0200

    add new route for Liga3

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from "react"
 import Liga12223 from "../pages/Liga1-22-23";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Liga22223 from "../pages/Liga2-22-23";
+import Liga32223 from "../pages/Liga3-22-23";
 import Home from "../pages/Home";
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
           <Route path="/liga1/*" element={<Liga12223 />} />
           <Route path="/liga2/22-23" element={<Liga22223 />} />
           <Route path="/liga2/*" element={<Liga22223 />} />
+          <Route path="/liga3/22-23" element={<Liga32223 />} />
+          <Route path="/liga3/*" element={<Liga32223 />} />
           <Route path="" element={<Home />} />
           <Route path="*" element={<Home />} />
         </Routes>
