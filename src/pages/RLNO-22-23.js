06a38bcad9dc78c804068093fb27abdc323b01a3
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-09-11 14:18:53 +0200

    add page for Regionalliga Nordost

---
+++ src/pages/RLNO-22-23.js
@@ -0,0 +1,23 @@
+/** @jsxImportSource theme-ui */
+import React from 'react'
+import Matches from '../components/Matches'
+import matches from '../data/matches_rlno_2022-23'
+import Table from '../components/Table'
+import { Link } from 'react-router-dom'
+
+function RLNO_22_23 () {
+  const separators = [0, 14, 15, 16]
+  return (
+    <header className='App-header'>
+      <div sx={{ display: "grid", gridTemplateColumns: "250px 1fr", columnGap: "20px", height: "calc(100vh - 50px)" }}>
+        <Matches matches={matches} />
+        <Table matches={matches} separators={separators} />
+      </div>
+      <div sx={{ width: "100%", height: "50px", margin: 0, textAlign: "center" }}>
+        <Link to="/">Zur Hauptseite</Link>
+      </div>
+    </header>
+  )
+}
+
+export default RLNO_22_23
