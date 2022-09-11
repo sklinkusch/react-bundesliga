fa4416b6b35427fa947ac53bd595f6e4f5e93e27
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-09-11 14:18:18 +0200

    add match data for Regionalliga Nordost

---
+++ src/data/matches_rlno_2022-23.js
@@ -0,0 +1,230 @@
+const matches = {
+  1: [
+    {
+      teams: ["LUC", "ERF"],
+      goals: [1, 1]
+    },
+    {
+      teams: ["L47", "HBS"],
+      goals: [2, 1]
+    },
+    {
+      teams: ["AGL", "ECB"],
+      goals: [2, 0]
+    },
+    {
+      teams: ["CZJ", "VBL"],
+      goals: [1, 1]
+    },
+    {
+      teams: ["CFC", "TBB"],
+      goals: [3, 0]
+    },
+    {
+      teams: ["ZFC", "BFC"],
+      goals: [1, 2]
+    },
+    {
+      teams: ["LKL", "BSC2"],
+      goals: [4, 2]
+    },
+    {
+      teams: ["BAK", "GFC"],
+      goals: [2, 0]
+    },
+    {
+      teams: ["SVB", "CHL"],
+      goals: [4, 2]
+    }
+  ],
+  2: [
+    {
+      teams: ["BSC2", "SVB"],
+      goals: [0, 1]
+    },
+    {
+      teams: ["VBL", "LKL"],
+      goals: [2, 2]
+    },
+    {
+      teams: ["CHL", "AGL"],
+      goals: [1, 2]
+    },
+    {
+      teams: ["ECB", "LUC"],
+      goals: [3, 0]
+    },
+    {
+      teams: ["GFC", "CFC"],
+      goals: [0, 0]
+    },
+    {
+      teams: ["TBB", "CZJ"],
+      goals: [0, 6]
+    },
+    {
+      teams: ["HBS", "ZFC"],
+      goals: [0, 3]
+    },
+    {
+      teams: ["ERF", "L47"],
+      goals: [5, 0]
+    },
+    {
+      teams: ["BFC", "BAK"],
+      goals: [0, 1]
+    }
+  ],
+  3: [
+    {
+      teams: ["CFC", "BFC"],
+      goals: [1, 0]
+    },
+    {
+      teams: ["SVB", "AGL"],
+      goals: [2, 2]
+    },
+    {
+      teams: ["LUC", "CHL"],
+      goals: [2, 2]
+    },
+    {
+      teams: ["LKL", "TBB"],
+      goals: [4, 0]
+    },
+    {
+      teams: ["BSC2", "VBL"],
+      goals: [3, 2]
+    },
+    {
+      teams: ["ZFC", "ERF"],
+      goals: [1, 4]
+    },
+    {
+      teams: ["BAK", "HBS"],
+      goals: [2, 0]
+    },
+    {
+      teams: ["CZJ", "GFC"],
+      goals: [1, 0]
+    },
+    {
+      teams: ["L47", "ECB"],
+      goals: [0, 0]
+    }
+  ],
+  4: [
+    {
+      teams: ["ERF", "BAK"],
+      goals: [0, 1]
+    },
+    {
+      teams: ["TBB", "BSC2"],
+      goals: [3, 3]
+    },
+    {
+      teams: ["BFC", "CZJ"],
+      goals: [0, 2]
+    },
+    {
+      teams: ["ECB", "ZFC"],
+      goals: [4, 0]
+    },
+    {
+      teams: ["CHL", "L47"],
+      goals: [3, 2]
+    },
+    {
+      teams: ["GFC", "LKL"],
+      goals: [3, 1]
+    },
+    {
+      teams: ["VBL", "SVB"],
+      goals: [0, 1]
+    },
+    {
+      teams: ["HBS", "CFC"],
+      goals: [1, 1]
+    },
+    {
+      teams: ["AGL", "LUC"],
+      goals: [2, 2]
+    }
+  ],
+  5: [
+    {
+      teams: ["LKL", "BFC"],
+      goals: [0, 0]
+    },
+    {
+      teams: ["CZJ", "HBS"],
+      goals: [2, 0]
+    },
+    {
+      teams: ["SVB", "LUC"],
+      goals: [1, 1]
+    },
+    {
+      teams: ["L47", "AGL"],
+      goals: [0, 1]
+    },
+    {
+      teams: ["VBL", "TBB"],
+      goals: [2, 1]
+    },
+    {
+      teams: ["CFC", "ERF"],
+      goals: [0, 3]
+    },
+    {
+      teams: ["BAK", "ECB"],
+      goals: [2, 0],
+      live: true
+    },
+    {
+      teams: ["BSC2", "GFC"],
+      goals: [1, 0],
+      live: true
+    },
+    {
+      teams: ["ZFC", "CHL"],
+      goals: [0, 0],
+      live: true
+    }
+  ],
+  6: [],
+  7: [],
+  8: [],
+  9: [],
+  10: [],
+  11: [],
+  12: [],
+  13: [],
+  14: [],
+  15: [],
+  16: [],
+  17: [],
+  18: [],
+  19: [],
+  20: [],
+  21: [],
+  22: [],
+  23: [],
+  24: [],
+  25: [],
+  26: [],
+  27: [],
+  28: [],
+  29: [],
+  30: [],
+  31: [],
+  32: [],
+  33: [],
+  34: [],
+  35: [],
+  36: [],
+  37: [],
+  38: []
+}
+
+export default matches
