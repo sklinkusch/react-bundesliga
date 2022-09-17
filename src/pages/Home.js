db8d657476362b03f775d05eadfaf6d86923091a
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-09-17 10:32:49 +0200

    add linking of women's league

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <header className='App-header' sx={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)' }}>
       <div>
-        <h2>1. Bundesliga</h2>
+        <h2>1. Bundesliga (M)</h2>
         <ul sx={{ listStyle: "none"}}>
           <li>
             <Link to="/liga1/22-23">Saison 2022/23</Link>
@@ -14,7 +14,15 @@ const Home = () => {
         </ul>
       </div>
       <div>
-        <h2>2. Bundesliga</h2>
+        <h2>1. Bundesliga (F)</h2>
+        <ul sx={{ listStyle: "none" }}>
+          <li>
+            <Link to="/wliga1/22-23">Saison 2022/23</Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h2>2. Bundesliga (M)</h2>
         <ul sx={{ listStyle: "none"}}>
           <li>
             <Link to="/liga2/22-23">Saison 2022/23</Link>
@@ -22,13 +30,13 @@ const Home = () => {
         </ul>
       </div>
       <div>
-        <h2>3. Liga</h2>
+        <h2>3. Liga (M)</h2>
         <ul sx={{ listStyle: "none" }}>
           <Link to="/liga3/22-23">Saison 2022/23</Link>
         </ul>
       </div>
       <div>
-        <h2>Regionalliga Nordost</h2>
+        <h2>Regionalliga Nordost (M)</h2>
         <ul>
           <Link to="/rlno/22-23">Saison 2022/23</Link>
         </ul>
