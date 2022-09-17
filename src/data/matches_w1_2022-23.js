2200d6fa900f7d29af2653a00a24199d6eb6c532
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-09-17 10:32:38 +0200

    add matches of women's league

---
+++ src/data/matches_w1_2022-23.js
@@ -0,0 +1,313 @@
+const matches = {
+  1: [
+    {
+      teams: ["SGE", "FCB"],
+      goals: [0, 0]
+    },
+    {
+      teams: ["WOB", "ESS"],
+      goals: [null, null]
+    },
+    {
+      teams: ["KÖL", "HOF"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SVM", "SCF"],
+      goals: [null, null]
+    },
+    {
+      teams: ["DUI", "B04"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SVW", "POT"],
+      goals: [null, null]
+    }
+  ],
+  2: [
+    {
+      teams: ["B04", "KÖL"],
+      goals: [null, null]
+    },
+    {
+      teams: ["HOF", "WOB"],
+      goals: [null, null]
+    },
+    {
+      teams: ["POT", "DUI"],
+      goals: [null, null]
+    },
+    {
+      teams: ["FCB", "SVW"],
+      goals: [null, null]
+    },
+    {
+      teams: ["ESS", "SVM"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SCF", "SGE"],
+      goals: [null, null]
+    }
+  ],
+  3: [
+    {
+      teams: ["WOB", "B04"],
+      goals: [null, null]
+    },
+    {
+      teams: ["KÖL", "POT"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SGE", "SVM"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SVM", "HOF"],
+      goals: [null, null]
+    },
+    {
+      teams: ["DUI", "FCB"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SCF", "ESS"],
+      goals: [null, null]
+    }
+  ],
+  4: [
+    {
+      teams: ["POT", "WOB"],
+      goals: [null, null]
+    },
+    {
+      teams: ["B04", "SVM"],
+      goals: [null, null],
+    },
+    {
+      teams: ["HOF", "SCF"],
+      goals: [null, null]
+    },
+    {
+      teams: ["ESS", "SGE"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SVW", "DUI"],
+      goals: [null, null]
+    },
+    {
+      teams: ["FCB", "KÖL"],
+      goals: [null, null]
+    }
+  ],
+  5: [
+    {
+      teams: ["SGE", "DUI"],
+      goals: [null, null]
+    },
+    {
+      teams: ["WOB", "FCB"],
+      goals: [null, null]
+    },
+    {
+      teams: ["KÖL", "SVW"],
+      goals: [null, null]
+    },
+    {
+      teams: ["ESS", "HOF"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SCF", "B04"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SVM", "POT"],
+      goals: [null, null]
+    }
+  ],
+  6: [
+    {
+      teams: ["POT", "SCF"],
+      goals: [null, null]
+    },
+    {
+      teams: ["B04", "ESS"],
+      goals: [null, null]
+    },
+    {
+      teams: ["HOF", "SGE"],
+      goals: [null, null]
+    },
+    {
+      teams: ["DUI", "KÖL"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SVW", "WOB"],
+      goals: [null, null]
+    },
+    {
+      teams: ["FCB", "SVM"],
+      goals: [null, null]
+    }
+  ],
+  7: [
+    {
+      teams: ["SGE", "KÖL"],
+      goals: [null, null]
+    },
+    {
+      teams: ["WOB", "DUI"],
+      goals: [null, null]
+    },
+    {
+      teams: ["HOF", "B04"],
+      goals: [null, null]
+    },
+    {
+      teams: ["ESS", "POT"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SCF", "FCB"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SVM", "SVW"],
+      goals: [null, null]
+    }
+  ],
+  8: [
+    {
+      teams: ["POT", "HOF"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SGE", "B04"],
+      goals: [null, null]
+    },
+    {
+      teams: ["KÖL", "WOB"],
+      goals: [null, null]
+    },
+    {
+      teams: ["DUI", "SVM"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SVW", "SCF"],
+      goals: [null, null]
+    },
+    {
+      teams: ["FCB", "ESS"],
+      goals: [null, null]
+    }
+  ],
+  9: [
+    {
+      teams: ["WOB", "SGE"],
+      goals: [null, null]
+    },
+    {
+      teams: ["B04", "POT"],
+      goals: [null, null]
+    },
+    {
+      teams: ["HOF", "FCB"],
+      goals: [null, null]
+    },
+    {
+      teams: ["ESS", "SVW"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SCF", "DUI"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SVM", "KÖL"],
+      goals: [null, null]
+    }
+  ],
+  10: [
+    {
+      teams: ["SGE", "POT"],
+      goals: [null, null]
+    },
+    {
+      teams: ["WOB", "SVM"],
+      goals: [null, null]
+    },
+    {
+      teams: ["KÖL", "SCF"],
+      goals: [null, null]
+    },
+    {
+      teams: ["DUI", "ESS"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SVW", "HOF"],
+      goals: [null, null]
+    },
+    {
+      teams: ["FCB", "B04"],
+      goals: [null, null]
+    }
+  ],
+  11: [
+    {
+      teams: ["POT", "FCB"],
+      goals: [null, null]
+    },
+    {
+      teams: ["B04", "SVW"],
+      goals: [null, null]
+    },
+    {
+      teams: ["HOF", "DUI"],
+      goals: [null, null]
+    },
+    {
+      teams: ["ESS", "KÖL"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SCF", "WOB"],
+      goals: [null, null]
+    },
+    {
+      teams: ["SVM", "SGE"],
+      goals: [null, null]
+    }
+  ],
+  12: [],
+  13: [],
+  14: [],
+  15: [],
+  16: [],
+  17: [],
+  18: [],
+  19: [],
+  20: [],
+  21: [],
+  22: [],
+  23: [],
+  24: [],
+  25: [],
+  26: [],
+  27: [],
+  28: [],
+  29: [],
+  30: [],
+  31: [],
+  32: [],
+  33: [],
+  34: []
+}
+
+export default matches
