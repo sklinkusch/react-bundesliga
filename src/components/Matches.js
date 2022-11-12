74cb61026de41750ae4f8e25f8565dd55965f6e7
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-11-12 14:10:28 +0100

    set prop date for Matches component

--- src/components/Matches.js
+++ src/components/Matches.js
@@ -17,15 +17,31 @@ const Matches = ({ matches }) => {
       <div>
         <h5 sx={{ margin: 0 }}>{selectedDay}. Spieltag</h5>
         {matches[selectedDay].map((match, index) => {
-          const { teams = [], goals = [], live = false } = match 
+          const { teams = [], goals = [], live = false, date = null } = match 
           const [home = null, away = null] = teams 
           const [homeGoals = null, awayGoals = null] = goals
+          if (typeof homeGoals === 'number' && typeof awayGoals === 'number' ) {
+            return (
+              <div sx={{ display: "flex", flexDirection: "row" }} key={`${home}-${away}-${index}`}>
+                {home ? <div sx={teamColumn}><Logo code={home} /></div> : <div sx={teamColumn}>&nbsp;</div>}
+                <span sx={{ flexBasis: "100px", flexGrow: 0, flexShrink: 0, textAlign: "center", color: live ? "blue" : "black", fontWeight: live ? "bold" : "normal" }}>{`${homeGoals}:${awayGoals}`}</span>
+                {away ? <div sx={{ flexBasis: "50px", flexGrow: 0, flexShrink: 0 }}><Logo code={away} /></div> : <div sx={teamColumn}>&nbsp;</div>}
+              </div>
+            )
+          }
+          if (homeGoals == null && awayGoals == null && typeof date === "string") {
+            return (
+              <div sx={{ display: "flex", flexDirection: "row" }} key={`${home}-${away}-${index}`}>
+                {home ? <div sx={teamColumn}><Logo code={home} /></div> : <div sx={teamColumn}>&nbsp;</div>}
+                <span sx={{ flexBasis: "100px", flexGrow: 0, flexShrink: 0}}>{date}</span>
+                {away ? <div sx={{ flexBasis: "50px", flexGrow: 0, flexShrink: 0 }}><Logo code={away} /></div> : <div sx={teamColumn}>&nbsp;</div>}
+              </div>
+            )  
+          }
           return (
             <div sx={{ display: "flex", flexDirection: "row" }} key={`${home}-${away}-${index}`}>
               {home ? <div sx={teamColumn}><Logo code={home} /></div> : <div sx={teamColumn}>&nbsp;</div>}
-              {typeof homeGoals === 'number' && typeof awayGoals === 'number' ? (
-                <span sx={{ flexBasis: "100px", flexGrow: 0, flexShrink: 0, textAlign: "center", color: live ? "blue" : "black", fontWeight: live ? "bold" : "normal" }}>{`${homeGoals}:${awayGoals}`}</span>
-                ) : (<span sx={{ flexBasis: "100px", flexGrow: 0, flexShrink: 0 }}>&nbsp;</span>)}
+              <span sx={{ flexBasis: "100px", flexGrow: 0, flexShrink: 0 }}>&nbsp;</span>
               {away ? <div sx={{ flexBasis: "50px", flexGrow: 0, flexShrink: 0 }}><Logo code={away} /></div> : <div sx={teamColumn}>&nbsp;</div>}
             </div>
           )})}
