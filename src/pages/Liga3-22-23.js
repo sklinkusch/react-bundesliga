0088c05126a2d2945d35f4ae8e0b4bb108e9913d
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-02-05 16:33:49 +0100

    set source for match data for Liga 3 (M) to api

--- src/pages/Liga3-22-23.js
+++ src/pages/Liga3-22-23.js
@@ -1,21 +1,34 @@
 /** @jsxImportSource theme-ui */
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Matches from '../components/Matches'
-import matches from '../data/matches_3_2022-23'
 import Table from '../components/Table'
 import { Link } from 'react-router-dom'
 /* eslint-disable react-hooks/exhaustive-deps */
 
 function Liga3_22_23 ({ title }) {
+  const [matches, setMatches] = useState({})
   useEffect(() => {
     document.title = title
   },[title])
+  useEffect(() => {
+    fetch('https://buli-api.vercel.app/liga3men?season=2022-23')
+    .then(response => response.json())
+    .then(data => {
+      if (typeof data === 'object' && Object.keys(data).length > 0) {
+        setMatches(data)
+      }
+    })
+  },[])
   const separators = [1, 2, 3, 15]
   return (
     <header className='App-header'>
       <div sx={{ display: "grid", gridTemplateColumns: "250px 1fr", columnGap: "20px", height: "calc(100vh - 50px)" }}>
-        <Matches matches={matches} />
-        <Table matches={matches} separators={separators} isThird={true} />
+        {typeof matches === 'object' && Object.keys(matches).length > 0 && (
+          <>
+            <Matches matches={matches} />
+            <Table matches={matches} separators={separators} isThird={true} />
+          </>
+        )}
       </div>
       <div sx={{ width: "100%", height: "50px", margin: 0, textAlign: "center" }}>
         <Link to="/">Zur Hauptseite</Link>
