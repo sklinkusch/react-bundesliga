50e1f48cdf42f3f9a79722a1d0c6b8f936f553f7
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-03-03 21:16:34 +0100

    RLNO, season 2022/23: adjust separators

--- src/pages/RLNO-22-23.js
+++ src/pages/RLNO-22-23.js
@@ -22,7 +22,7 @@ function RLNO_22_23 ({ title }) {
       }
     })
   },[])
-  const separators = [0, 14, 15, 16]
+  const separators = [0, 13]
   return (
     <header className='App-header'>
       <div sx={{ display: "grid", gridTemplateColumns: "250px 1fr", columnGap: "20px", height: "calc(100vh - 50px)" }}>
