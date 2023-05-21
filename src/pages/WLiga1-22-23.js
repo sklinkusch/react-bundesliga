c848a5ef0b07c08c6e563b5cdddf90d9f4d968a8
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-05-21 20:28:54 +0200

    add colors for Liga 1 Women

--- src/pages/WLiga1-22-23.js
+++ src/pages/WLiga1-22-23.js
@@ -11,12 +11,15 @@ function WLiga1_22_23 ({ title }) {
   const [queryParams] = useSearchParams()
   const [possible, setPossible] = useState([])
   const [colors, setColors] = useState([])
+  const [fcolors, setFColors] = useState([])
   const navigate = useNavigate()
   useEffect(() => {
     document.title = title
   },[title])
   useEffect(() => {
-    fetch('https://buli-api.vercel.app/liga1women?season=2022-23')
+    // const url = 'https://buli-api.vercel.app/liga1women?season=2022-23'
+    const url = 'http://localhost:3500/liga1women?season=2022-23'
+    fetch(url)
     .then(response => response.json())
     .then(data => {
       if (typeof data === 'object' && Object.keys(data).length > 0) {
@@ -107,20 +110,37 @@ function WLiga1_22_23 ({ title }) {
       function getColors() {
         const myColors = Array.isArray(possible) && possible.length > 0 ? possible.map(team => {
           // Meister und Gruppenphase der Champions League
-          if (team.worst === 0) return "#F9A800"
+          if (team.worst === 0) return "linear-gradient(to bottom, #e6f0a3 0%, #d2e638 50%, #c3d825 51%, #dbf043 100%)"
           // 2. Qualifikationsrunde der Champions League
-          if (team.worst <= 1) return "#FFFF00"
+          if (team.worst <= 1) return "linear-gradient(to bottom, #f6f8f9 0%, #e5ebee 50%, #d7dee3 51%, #f5f7f9 100%)"
           // 1. Qualifikationsrunde der Champions League
-          if (team.worst <= 2) return "#CBD0CC"
+          if (team.worst <= 2) return "linear-gradient(to bottom, #f3e2c7 0%, #c19e67 50%, #b68d4c 51%, #e9d4b3 100%)"
           // Klassenerhalt
-          if (team.worst <= 9) return "#D2E2D0"
+          if (team.worst <= 9) return "linear-gradient(to bottom, #d2ff52 0%, #91e842 100%)"
           // Abstieg
-          if (team.best >= 10) return "#D79FA6"
+          if (team.best >= 10) return "linear-gradient(to bottom, #ff3019 0%, #c40404 100%)"
           return "none"
         }) : []
         setColors(myColors)
       }
+      function getFontColors() {
+        const myFColors = Array.isArray(possible) && possible.length > 0 ? possible.map(team => {
+          // Meister und Gruppenphase der Champions League
+          if (team.worst === 0) return "black"
+          // 2. Qualifikationsrunde der Champions League
+          if (team.worst <= 1) return "black"
+          // 1. Qualifikationsrunde der Champions League
+          if (team.worst <= 2) return "white"
+          // Klassenerhalt
+          if (team.worst <= 9) return "black"
+          // Abstieg
+          if (team.best >= 10) return "white"
+          return "black"
+        }) : []
+        setFColors(myFColors)
+      }
       getColors()
+      getFontColors()
   },[possible])
   return (
     <header className='App-header'>
@@ -128,7 +148,7 @@ function WLiga1_22_23 ({ title }) {
         { typeof matches === 'object' && Object.keys(matches).length > 0 && (
           <>
             {Object.keys(matches).length > 0 && <Matches matches={matches} selDay={queryParams.get("day")} source="/wliga1/22-23" />}
-            {table.length > 0 && <Table table={table} separators={separators} colors={colors} />}
+            {table.length > 0 && <Table table={table} separators={separators} colors={colors} fontcolors={fcolors} />}
           </>
         )}
       </div>
