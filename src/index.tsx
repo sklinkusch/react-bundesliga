f3072941b3975ab2425e0c8a04e70c835a6ef5f6
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-06-17 15:33:42 +0200

    include index.css in index.html

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import "./styles/index.css"
 import App from "./components/App"
 import reportWebVitals from "./reportWebVitals"
 import { ThemeProvider } from "@theme-ui/core"
