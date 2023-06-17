aacb183deee56793d46dd9ad2d16e7cffe74fe0a
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-06-17 14:06:59 +0200

    translate theme to typescript

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,18 @@
-const theme = {
+type Theme = {
+  breakpoints: string[]
+  container: string[]
+  fonts: {
+    body: string
+  }
+  colors: { [key: string]: string }
+}
+
+const theme: Theme = {
   breakpoints: ["576px", "768px", "992px", "1200px"],
   container: ["540px", "720px", "960px", "1140px"],
   fonts: {
     body: `-apple-system, "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu",
-    "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif`,
+    "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif`
   },
   colors: {
     bblue: "blue",
