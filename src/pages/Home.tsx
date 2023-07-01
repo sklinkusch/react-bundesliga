ae36b0a621db6d835a88cef664853e672c8497c3
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-07-01 17:34:57 +0200

    link page for Liga 1 season 2023/24

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,6 +20,9 @@ const Home = ({ title }: Props) => {
           <h3>1. Bundesliga (♂)</h3>
           <ul sx={{ listStyle: "none" }}>
             <li>
+              <Link to="/liga1/23-24">Saison 2023/24</Link>
+            </li>
+            <li>
               <Link to="/liga1/22-23">Saison 2022/23</Link>
             </li>
           </ul>
