ed86b515f66ebaa63e72902a9be52a196c1989b1
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-08-26 17:16:01 +0200

    automatic selection of url for Liga 3 Men, season 2022/23

--- src/pages/Liga3-22-23.tsx
+++ src/pages/Liga3-22-23.tsx
@@ -48,8 +48,11 @@ function Liga3_22_23({ title }: Props) {
     document.title = title
   }, [title])
   useEffect(() => {
-    const url = "https://buli-api.vercel.app/liga3men?season=2022-23"
-    // const url = 'http://localhost:3500/liga3men?season=2022-23'
+    const baseUrl =
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3500"
+        : "https://buli-api.vercel.app"
+    const url = baseUrl + "/liga3men?season=2022-23"
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
